1261c8ef85b44292423ec64110d5d387f7b3a723
R1cs1KING <ricsi960413@gmail.com>
2022-06-17 14:22:05 +0200

    added initial form

--- src/app/maze/maze.component.ts
+++ src/app/maze/maze.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-maze',
   templateUrl: './maze.component.html',
-  styleUrls: ['./maze.component.css']
+  styleUrls: ['./maze.component.css'],
+  providers: [FormBuilder]
 })
 export class MazeComponent implements OnInit {
 
-  constructor() { }
+  isGameOn:boolean = false;
+  public mazeForm: FormGroup;
+  mazeId:string = "";
+
+  constructor(fb: FormBuilder) {
+    this.mazeForm = fb.group({
+      MazeWidth : new FormControl(Validators.compose([Validators.required, Validators.min(15), Validators.max(25)])),
+      MazeHeight : new FormControl(Validators.compose([Validators.required, Validators.min(15), Validators.max(25)])),
+      MazePlayerName: new FormControl(Validators.compose([Validators.required])),
+      Difficulty: new FormControl(Validators.compose([Validators.min(1), Validators.max(10)]))
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submitMaze() {
+
   }
 
 }
